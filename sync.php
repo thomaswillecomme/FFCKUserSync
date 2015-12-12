d3847e968102e507aef1d876215c09d8e6bf5fec
Thomas Willecomme <thomas.willecomme@laposte.net>
2015-12-12 14:45:56 +0100

    fixed login failed

--- sync.php
+++ sync.php
@@ -8,93 +8,109 @@ class ffck_connector{
     var $password;
     var $token;
     var $cookie_file;
+    var $session;
     
     function __construct($login, $password){
         $this->login = $login;
         $this->password = $password;
         $this->cookie_file = dirname(__FILE__).'/cookie.txt';
+        unlink($this->cookie_file);
     }
     
-    //first we need a valid token which is hidden inside the html form
-    //fetch the token to use for login request
-    function get_token(){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, 'https://ffck-goal.multimediabs.com/login');
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
-        $result = curl_exec($ch);
-
-        $pattern ='<input type="hidden" name="authenticityToken" value="(?P<token>.*?)">';
-        preg_match($pattern, $result, $matches, PREG_OFFSET_CAPTURE, 3);
-        $this->token= $matches['token'][0];
-    }
-    
-    //second we get a temporary session for login purpose (token, username and password are not used by remote server)
+   
+    //we get a temporary session for login purpose (token, username and password are not used by remote server)
     function get_session(){
+        echo '#fetching temporary session and token'.PHP_EOL;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://ffck-goal.multimediabs.com/login');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: ffck-goal.multimediabs.com', 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'));
         curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
         curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, FALSE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         // get headers too with this line
         curl_setopt($ch, CURLOPT_HEADER, 1);
         $result = curl_exec($ch);
         // get cookie
         // multi-cookie variant contributed by @Combuster in comments
-        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
+        $cookies = $this->get_cookies($result);
+        $this->session = $cookies['FFCK_SESSION'];
+        echo 'got session '.$cookies['FFCK_SESSION'].PHP_EOL;
+        
+        $pattern ='<input type="hidden" name="authenticityToken" value="(?P<token>.*?)">';
+        preg_match($pattern, $result, $matches, PREG_OFFSET_CAPTURE, 3);
+        $this->token= $matches['token'][0];
+        echo 'got token '.$this->token.PHP_EOL;
+    }
+    
+    
+    function get_cookies($header){
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $header, $matches);
         $cookies = array();
         foreach($matches[1] as $item) {
             parse_str($item, $cookie);
             $cookies = array_merge($cookies, $cookie);
         }
+        
+        return $cookies;
     }
     
     //third we run again the request with the temporary session id, login, password and token. it will return an actual session id
     function login(){
-        $this->get_token();
+        //$this->get_token();
         $this->get_session();
         
+        echo '#Login in'.PHP_EOL;
         $this->flash='%00previousUrl%3A%2F%00%00url%3A%2F%00';
-        $this->lb='was03';
+        $this->lb='was02';
         
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://ffck-goal.multimediabs.com/login');
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: ffck-goal.multimediabs.com', 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'));
         curl_setopt($ch, CURLOPT_POST, 1);
         curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('authenticityToken' => $this->token, 'username' => $this->login, 'password'=> $this->password)));
-        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
         curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
-        // get headers too with this line
         curl_setopt($ch, CURLOPT_HEADER, 1);
+        // get headers too with this line
+
         $result = curl_exec($ch);
-        // get cookie
-        // multi-cookie variant contributed by @Combuster in comments
-        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $result, $matches);
-        $cookies = array();
-        foreach($matches[1] as $item) {
-            parse_str($item, $cookie);
-            $cookies = array_merge($cookies, $cookie);
+        
+        $cookies = $this->get_cookies($result);
+        
+        
+        if(strcmp($this->session,$cookies['FFCK_SESSION']) == 0){
+            echo 'session not updated, login failed'.PHP_EOL;
+            var_dump($result);
+            return FALSE;
+        }else{
+            echo 'session updated ('.$cookies['FFCK_SESSION'].'), logged in'.PHP_EOL;
+            return TRUE;
         }
     }
+
     
     function get_members($season){
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, 'https://ffck-goal.multimediabs.com/licences/afficherlistelicencies?idSaison='.$season);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array('Host: ffck-goal.multimediabs.com', 'Accept: application/json; q=0.01'));
-        //curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
         curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_file);
         curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_file);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
+        //curl_setopt($ch, CURLOPT_HEADER, 1);
         $result = curl_exec($ch);
+
+        
+
         $php_result = json_decode($result,true);
+        curl_close($ch);
         return $php_result['licences'];
+
+        
+
     }
     
     function get_member_data($licence_id){
@@ -188,15 +204,20 @@ class ffck_connector{
 
 function ffck_sync(){
     //FIXME unsecure
-    $connector = new ffck_connector($_POST['login'],$_POST['password']);
+    $connector = new ffck_connector($_POST['user'],$_POST['password']);
 
-    echo 'logging in...';
-    $connector->login();
-    echo 'done'.PHP_EOL;
+    if($connector->login() == FALSE){
+        return;
+    }
 
+
+   
     //FIXME unsecure
     echo 'fetching users list...';
     $members = $connector->get_members($_POST['season']);
+    if($members == FALSE){
+        return;
+    }
     echo 'done : found '.count($members).PHP_EOL;
 
     echo 'fetching users informations...'.PHP_EOL;
